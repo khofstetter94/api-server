7ca6e848e58a3ee9a03f0fe8a91893e5bea9459c
KC Hofstetter <kclynhofstetter@gmail.com>
2022-09-22 22:57:47 -0700

    added heroku deployment postgres config details

--- src/models/index.js
+++ src/models/index.js
@@ -1,6 +1,8 @@
 'use strict';
 
 require('dotenv').config();
+const POSTGRES_URI = process.env.DATABASE_URL;
+
 const { Sequelize, DataTypes } = require('sequelize');
 const foodsSchema = require('./foods.schema');
 const clothesSchema = require('./clothes.schema');
@@ -8,7 +10,16 @@ const ModelInterface = require('./ModelInterface');
 
 const DATABASE_URL = process.env.NODE_ENV === 'test' ? 'sqlite:memory' : process.env.DATABASE_URL;
 
-const sequelizeDatabase = new Sequelize(DATABASE_URL);
+const sequelizeDatabase = new Sequelize(
+  POSTGRES_URI || DATABASE_URL, {
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  },
+);
 
 const FoodsModel = foodsSchema(sequelizeDatabase, DataTypes);
 const ClothesModel = clothesSchema(sequelizeDatabase, DataTypes);
